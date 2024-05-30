4e89465398ebc22f641fdeebe17b8c41be064457
code-grafiki <mkishore3002@gmail.com>
2024-05-30 22:19:28 +0530

    add coroutine for level switching delay

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    [SerializeField] float LoadLevelDelay = 0;
     void OnTriggerEnter2D(Collider2D other)
     {
+        StartCoroutine(LoadNextLevel());  
+    }
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LoadLevelDelay);
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex+1);
     }
