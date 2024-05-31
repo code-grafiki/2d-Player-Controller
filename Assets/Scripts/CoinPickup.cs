84a2ea844d0b60a2499afe7a5281fea6b520f474
code-grafiki <mkishore3002@gmail.com>
2024-05-31 20:43:15 +0530

    add coinPickUp.sfx

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUp;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") 
         {
+            AudioSource.PlayClipAtPoint(coinPickUp, Camera.main.transform.position);
             Destroy(gameObject);
             Debug.Log("coin picked up");
         }
+        
     }
 }
